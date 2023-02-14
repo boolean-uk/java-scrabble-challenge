a35c646d617a83c90075980597158b37928a0f60
Nathan King <nathanking3590@gmail.com>
2023-02-14 10:54:44 +0000

    add exercise

---
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -0,0 +1,5 @@
+package com.booleanuk;
+
+public class Scrabble {
+
+}
---
+++ src/test/java/com/booleanuk/ScrabbleTest.java
@@ -0,0 +1,47 @@
+package com.booleanuk;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ScrabbleTest {
+    @Test
+    void shouldGive0ForEmptyWords() {
+        Scrabble scrabble = new Scrabble("");
+        Assertions.assertEquals(0, scrabble.score());
+    }
+
+    @Test
+    void shouldGive0ForWhiteSpace() {
+        Scrabble scrabble = new Scrabble(" \t\s");
+        Assertions.assertEquals(0, scrabble.score());
+    }
+
+    @Test
+    void shouldScore1ForA() {
+        Scrabble scrabble = new Scrabble("a");
+        Assertions.assertEquals(1, scrabble.score());
+    }
+
+    @Test
+    void shouldScore4ForF() {
+        Scrabble scrabble = new Scrabble("f");
+        Assertions.assertEquals(4, scrabble.score());
+    }
+
+    @Test
+    void shouldScore6ForStreet() {
+        Scrabble scrabble = new Scrabble("street");
+        Assertions.assertEquals(6, scrabble.score());
+    }
+
+    @Test
+    void shouldScore22ForQuirky() {
+        Scrabble scrabble = new Scrabble("quirky");
+        Assertions.assertEquals(22, scrabble.score());
+    }
+
+    void shouldScore41ForCaseInsensitiveWord() {
+        Scrabble scrabble = new Scrabble("OXyPHEnBUTaZoNE");
+        Assertions.assertEquals(41, scrabble.score());
+    }
+}
