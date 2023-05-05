a1ef5b110d0069e106207835d9e1cf5c66446b35
dtsimaras <tsimaras.dimitrios@gmail.com>
2023-05-06 02:17:15 +0300

    This got out of hand

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -2,51 +2,89 @@ package com.booleanuk;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
+import java.util.stream.Collectors;
 
 public class Scrabble {
     private final String word;
-    private static final Map<Character, Integer> valueOfLetter;
 
-    static {
-        valueOfLetter = new HashMap<>();
-        valueOfLetter.put('A', 1);
-        valueOfLetter.put('E', 1);
-        valueOfLetter.put('I', 1);
-        valueOfLetter.put('O', 1);
-        valueOfLetter.put('U', 1);
-        valueOfLetter.put('L', 1);
-        valueOfLetter.put('N', 1);
-        valueOfLetter.put('R', 1);
-        valueOfLetter.put('S', 1);
-        valueOfLetter.put('T', 1);
-        valueOfLetter.put('D', 2);
-        valueOfLetter.put('G', 2);
-        valueOfLetter.put('B', 3);
-        valueOfLetter.put('C', 3);
-        valueOfLetter.put('M', 3);
-        valueOfLetter.put('P', 3);
-        valueOfLetter.put('F', 4);
-        valueOfLetter.put('H', 4);
-        valueOfLetter.put('V', 4);
-        valueOfLetter.put('W', 4);
-        valueOfLetter.put('Y', 4);
-        valueOfLetter.put('K', 5);
-        valueOfLetter.put('J', 8);
-        valueOfLetter.put('X', 8);
-        valueOfLetter.put('Q', 10);
-        valueOfLetter.put('Z', 10);
-    }
-
-    public Scrabble (String word) {
-        this.word = word.trim();
+    public Scrabble(String word) {
+        this.word = word.trim().toLowerCase();
     }
 
     public int score() {
-        int value = 0;
-        String wordInUpperCase = word.toUpperCase();
-        for (int i=0; i < word.length();i++) {
-            value += valueOfLetter.get(wordInUpperCase.charAt(i));
+        if (!isWordValid()) return 0;
+
+        int score = 0;
+        int multiplier = 1;
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+
+            if (c >= 'a' && c <= 'z') {
+                score += getCharValue(c) * multiplier;
+            } else if (c == '[' || c == '{') {
+                multiplier *= getCharValue(c);
+            } else if (c == ']' || c == '}') {
+                multiplier /= getCharValue(c);
+            }
         }
-        return value;
+        if (multiplier != 1) return 0;
+        return score;
+    }
+
+    private static int getCharValue(char c) {
+        return switch (c) {
+            case 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't' -> 1;
+            case 'd', 'g', '{', '}' -> 2;
+            case 'b', 'c', 'm', 'p', '[', ']' -> 3;
+            case 'f', 'h', 'v', 'w', 'y' -> 4;
+            case 'k' -> 5;
+            case 'j', 'x' -> 8;
+            case 'q', 'z' -> 10;
+            default -> throw new IllegalArgumentException("Invalid character: " + c);
+        };
+    }
+
+    public boolean isWordValid() {
+        boolean doubleBracketsAroundLetter = word.matches(".*[{\\[]{2}+[a-z][}\\]]{2}+.*");
+        if (doubleBracketsAroundLetter) return false;
+
+        Stack<Character> stack = new Stack<>();
+        Stack<Integer> chars = new Stack<>(); //Letters in Between Brackets
+        int numberOfLetters = word.replaceAll("[{}\\[\\]]", "").length();
+
+        for (char c : word.toCharArray()) {
+            switch (c) {
+                case '{', '[' -> {
+                    stack.push(c);
+                    chars.push(0);
+                }
+                case '}' -> {
+                    if (stack.isEmpty() || stack.peek() != '{' ||
+                            (chars.peek() != 1 && chars.peek() != numberOfLetters)) {
+                        return false;
+                    }
+                    stack.pop();
+                    chars.pop();
+                }
+                case ']' -> {
+                    if (stack.isEmpty() || stack.peek() != '[' ||
+                            (chars.peek() != 1 && chars.peek() != numberOfLetters)) {
+                        return false;
+                    }
+                    stack.pop();
+                    chars.pop();
+                }
+                default -> {
+                    if (Character.isLetter(c)) {
+                        chars = chars.stream().map(i -> i + 1).collect(Collectors.toCollection(Stack::new));
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
     }
 }
