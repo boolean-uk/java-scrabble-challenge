7b80b2cf643ec57311e979791c20b178edf960b0
Kitti <kitti.horvath6891@gmail.com>
2023-05-10 18:02:54 +0200

    scrabble completed

--- src/test/java/com/booleanuk/ScrabbleTest.java
+++ src/test/java/com/booleanuk/ScrabbleTest.java
@@ -3,6 +3,7 @@ package com.booleanuk;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+
 class ScrabbleTest {
     @Test
     public void shouldGive0ForEmptyWords() {
@@ -45,4 +46,42 @@ class ScrabbleTest {
         Scrabble scrabble = new Scrabble("OXyPHEnBUTaZoNE");
         Assertions.assertEquals(41, scrabble.score());
     }
+
+    @Test
+    public void shouldScore6ForDogWithDoubleLetter() {
+        Scrabble scrabble = new Scrabble("D{o}g");
+        Assertions.assertEquals(6, scrabble.score());
+    }
+
+    @Test
+    public void shouldScore9ForDogWithTrippleLetter() {
+        Scrabble scrabble = new Scrabble("[D]og");
+        Assertions.assertEquals(9, scrabble.score());
+    }
+
+    @Test
+    public void shouldScore10ForDogWithDoubleWordScore(){
+        Scrabble scrabble = new Scrabble("{Dog}");
+        Assertions.assertEquals(10, scrabble.score());
+    }
+
+    @Test
+    public void shouldScore15ForDogWithTrippleWordScore(){
+        Scrabble scrabble = new Scrabble("[Dog]");
+        Assertions.assertEquals(15, scrabble.score());
+    }
+
+    @Test
+    public void shouldScore10ForDogWithDoubleAndTrippleLetter(){
+        Scrabble scrabble = new Scrabble("[D]{o}g");
+        Assertions.assertEquals(10, scrabble.score());
+    }
+
+    @Test
+    public void shouldScore0ForDogWrongBracets(){
+        Scrabble scrabble = new Scrabble("[D}o}g");
+        Assertions.assertEquals(0, scrabble.score());
+    }
+
+
 }
