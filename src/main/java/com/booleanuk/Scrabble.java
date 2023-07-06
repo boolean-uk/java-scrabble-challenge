0785b8fc3cb7bf0faa688c6ff6d596be1863d3ec
Marcin Stefanowicz <marcin.stefanowicz@talan.com>
2023-07-06 14:20:27 +0200

    first commit

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -1,5 +1,26 @@
 package com.booleanuk;
 
 public class Scrabble {
+    String word;
 
+    public Scrabble(String word) {
+        this.word = word;
+    }
+
+    public int score(){
+        int points = 0;
+
+        for (int i = 0; i < this.word.length(); i++) {
+            if(Character.toString(this.word.charAt(i)).toUpperCase().matches("[AEIOULNRST]")) points++;
+            else if(Character.toString(this.word.charAt(i)).toUpperCase().matches("[DG]")) points += 2;
+            else if(Character.toString(this.word.charAt(i)).toUpperCase().matches("[BCMP]")) points += 3;
+            else if(Character.toString(this.word.charAt(i)).toUpperCase().matches("[FHVWY]")) points += 4;
+            else if(Character.toString(this.word.charAt(i)).toUpperCase().matches("[K]")) points += 5;
+            else if(Character.toString(this.word.charAt(i)).toUpperCase().matches("[JX]")) points += 8;
+            else if(Character.toString(this.word.charAt(i)).toUpperCase().matches("[QZ]")) points += 10;
+
+        }
+
+        return points;
+    }
 }
