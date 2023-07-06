90e33f5f498f112504136dba43003576755941d1
Michał Klukowski <michal.klukowski@talan.com>
2023-07-06 16:36:02 +0200

    Done extended criteria

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -8,8 +8,12 @@ public class Scrabble {
     private final Map<String, Integer> letters = new HashMap<>();
     private final String word;
 
+
     public Scrabble(String word) {
         this.word = word.trim().toUpperCase();
+
+        if(!isValid(this.word))
+            throw new IllegalArgumentException("No matching brackets.");
 
         Map<String, Integer> letters = Map.of(
                 "A, E, I, O, U, L, N, R, S, T", 1,
@@ -30,10 +34,38 @@ public class Scrabble {
 
     public int score() {
         int score = 0;
+        int multiplier = 1;
         for (String letter : word.split("")) {
-            score += letters.getOrDefault(letter, 0);
+            multiplier *= switch (letter) {
+                case "{" -> 2;
+                case "[" -> 3;
+                default -> 1;
+            };
+            multiplier /=  switch (letter) {
+                case "}" -> 2;
+                case "]" -> 3;
+                default -> 1;
+            };
+            score += letters.getOrDefault(letter, 0) * multiplier;
         }
         return score;
+    }
+
+    private boolean isValid(String word) {
+        int curly = 0;
+        int square = 0;
+        for(String letter : word.split("")) {
+            switch (letter) {
+                case "{" -> curly++;
+                case "}" -> curly--;
+                case "[" -> square++;
+                case "]" -> square--;
+            }
+            if(curly < 0 || square < 0)
+                return false;
+        }
+
+        return curly == 0 && square == 0;
     }
 }
 
