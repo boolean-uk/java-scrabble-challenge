718848ed2146d720175dd922f48a0b8bb5b08803
PiotrSadolewski <piotr.sadolewski00@gmail.com>
2023-07-06 13:17:32 +0200

    Scrabble class created

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -1,5 +1,50 @@
 package com.booleanuk;
 
-public class Scrabble {
+import java.util.Map;
 
+public class Scrabble {
+    public String wordToCalculate;
+    public int score;
+    public static final Map<Character, Integer> pointsForLetters = Map.ofEntries(
+            Map.entry('A', 1),
+            Map.entry('E', 1),
+            Map.entry('I', 1),
+            Map.entry('O', 1),
+            Map.entry('U', 1),
+            Map.entry('L', 1),
+            Map.entry('N', 1),
+            Map.entry('R', 1),
+            Map.entry('S', 1),
+            Map.entry('T', 1),
+            Map.entry('D', 2),
+            Map.entry('G', 2),
+            Map.entry('B', 3),
+            Map.entry('C', 3),
+            Map.entry('M', 3),
+            Map.entry('P', 3),
+            Map.entry('F', 4),
+            Map.entry('H', 4),
+            Map.entry('V', 4),
+            Map.entry('W', 4),
+            Map.entry('Y', 4),
+            Map.entry('K', 5),
+            Map.entry('J', 8),
+            Map.entry('X', 8),
+            Map.entry('Q', 10),
+            Map.entry('Z', 10)
+    );
+
+    public Scrabble(String wordToCalculate) {
+        this.wordToCalculate = wordToCalculate.toUpperCase();
+    }
+
+    public int score(){
+        char[] arrayOfChars = this.wordToCalculate.toCharArray();
+        for (char c : arrayOfChars){
+            if (pointsForLetters.containsKey(c)){
+                this.score += pointsForLetters.get(c);
+            }
+        }
+        return score;
+    }
 }
