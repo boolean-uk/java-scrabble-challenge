8a7c0cb4512e18c8d1e28bce5d55f071e56740b8
Kornel <korneldabrowski@gmail.com>
2023-07-06 15:08:00 +0200

    adding extension tests

--- src/test/java/com/booleanuk/ScrabbleTest.java
+++ src/test/java/com/booleanuk/ScrabbleTest.java
@@ -45,4 +45,72 @@ class ScrabbleTest {
         Scrabble scrabble = new Scrabble("OXyPHEnBUTaZoNE");
         Assertions.assertEquals(41, scrabble.score());
     }
+
+    @Test
+    public void doublePointsLetter() {
+        Scrabble scrabble = new Scrabble("d{o}g");
+        Assertions.assertEquals(6, scrabble.score());
+    }
+
+    @Test
+    public void triplePointsLetter() {
+        Scrabble scrabble = new Scrabble("d[o]g");
+        Assertions.assertEquals(7, scrabble.score());
+    }
+
+    @Test
+    public void doublePointsWord() {
+        Scrabble scrabble = new Scrabble("{dog}");
+        Assertions.assertEquals(10, scrabble.score());
+    }
+
+    @Test
+    public void triplePointsWord() {
+        Scrabble scrabble = new Scrabble("[dog]");
+        Assertions.assertEquals(15, scrabble.score());
+    }
+
+    @Test
+    public void mix() {
+        Scrabble scrabble = new Scrabble("[d]o{g}");
+        Assertions.assertEquals(11, scrabble.score());
+    }
+
+    @Test
+    public void unevenBrackets() {
+        Scrabble scrabble = new Scrabble("[dog}");
+        Assertions.assertEquals(0, scrabble.score());
+        scrabble = new Scrabble("[dog");
+        Assertions.assertEquals(0, scrabble.score());
+        scrabble = new Scrabble("dog}");
+        Assertions.assertEquals(0, scrabble.score());
+        scrabble = new Scrabble("[[dog");
+        Assertions.assertEquals(0, scrabble.score());
+        scrabble = new Scrabble("[[}}");
+        Assertions.assertEquals(0, scrabble.score());
+    }
+
+    @Test
+    public void letterWordMultiply() {
+        Scrabble scrabble = new Scrabble("[d{o}g]");
+        Assertions.assertEquals(18, scrabble.score());
+        scrabble = new Scrabble("[{d}[o]g]");
+        Assertions.assertEquals(27, scrabble.score());
+    }
+
+    @Test
+    public void badBrackets() {
+        Scrabble scrabble = new Scrabble("[do]g");
+        Assertions.assertEquals(0, scrabble.score());
+        scrabble = new Scrabble("{dooooooo}g");
+        Assertions.assertEquals(0, scrabble.score());
+    }
+
+    @Test
+    public void incorrectWord() {
+        Scrabble scrabble = new Scrabble("8888d");
+        Assertions.assertEquals(0, scrabble.score());
+        scrabble = new Scrabble("===**dog");
+        Assertions.assertEquals(0, scrabble.score());
+    }
 }
