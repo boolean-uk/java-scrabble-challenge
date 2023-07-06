104b4ed7f375e560f16b82d7496828766aa15e41
Julia Girejko <julia.girejko@talan.com>
2023-07-06 16:20:02 +0200

    acceptance criteria

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -1,5 +1,60 @@
 package com.booleanuk;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
 public class Scrabble {
+
+    private String word;
+    private HashMap<Character, Integer> letterScores = new HashMap<>();
+    public Scrabble(String word) {
+        this.word = word;
+
+        letterScores.put('A', 1);
+        letterScores.put('E', 1);
+        letterScores.put('I', 1);
+        letterScores.put('O', 1);
+        letterScores.put('U', 1);
+        letterScores.put('L', 1);
+        letterScores.put('N', 1);
+        letterScores.put('R', 1);
+        letterScores.put('S', 1);
+        letterScores.put('T', 1);
+        letterScores.put('D', 2);
+        letterScores.put('G', 2);
+        letterScores.put('B', 3);
+        letterScores.put('C', 3);
+        letterScores.put('M', 3);
+        letterScores.put('P', 3);
+        letterScores.put('F', 4);
+        letterScores.put('H', 4);
+        letterScores.put('V', 4);
+        letterScores.put('W', 4);
+        letterScores.put('Y', 4);
+        letterScores.put('K', 5);
+        letterScores.put('J', 8);
+        letterScores.put('X', 8);
+        letterScores.put('Q', 10);
+        letterScores.put('Z', 10);
+
+    }
+
+    public int score() {
+        int result = 0;
+        char[] letters = word.toCharArray();
+
+        for (char c : letters){
+            c = Character.toUpperCase(c);
+            if(letterScores.containsKey(c)){
+                result += letterScores.get(c);
+            }
+        }
+
+        return result;
+    }
+
 
 }
