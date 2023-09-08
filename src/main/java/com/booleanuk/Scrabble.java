eb3fcc0c84ee92cc8ab420924cde1fbc88ee10c0
Ali Keskin <cmdalikeskin@gmail.com>
2023-09-08 02:08:16 +0200

    Update Scrabble.java

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -1,5 +1,29 @@
 package com.booleanuk;
 
+
 public class Scrabble {
+
+    public String strVal = "empty string";
+
+    public  Scrabble(String test) {
+        this.strVal = test;
+    }
+
+
+
+
+    public int score() {
+        char[] calcVal = strVal.toCharArray();
+
+        System.out.println(calcVal[4]);
+
+        for(int i =0 ; i < calcVal.length; i++){
+
+        }
+
+
+
+        return 0;
+    }
 
 }
