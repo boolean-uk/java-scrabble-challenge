6333c4b81cbea59a1833d8178982e485e0fe119b
Ali Keskin <cmdalikeskin@gmail.com>
2023-09-10 04:39:41 +0200

    Got a eureka moment 4 in the morning and added the extension to the exercise.

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -4,6 +4,7 @@ package com.booleanuk;
 public class Scrabble {
 
     public String strVal = "";
+    public int multiplier = 1;
 
     public Scrabble(String test) {
         this.strVal = test;
@@ -12,7 +13,7 @@ public class Scrabble {
     public int score() {
         char[] calcVal = strVal.toCharArray();
         int score = 0;
-            //  System.out.println(calcVal[4]);
+        //  System.out.println(calcVal[4]);
 
         for (char c : calcVal) {
             String checkerValue = String.valueOf(c).toUpperCase();
@@ -21,21 +22,34 @@ public class Scrabble {
             //  checkerValue.equals("E") ||
             //  checkerValue.equals("I")
 
+            /*
+             * I added the checks to see if a letter/word should get double or triple score at the end
+             * of the if-statement list. I did not add checks to see if a user error has been put (i.e. someone put }
+             * before { ) as that wasn't asked in the exercise.
+             * */
+
 
             if ((checkerValue.matches("[AEIOULNRST]"))) {
-                score += 1;
+                score += 1 * multiplier;
             } else if (checkerValue.matches("[DG]")) {
-                score += 2;
+                score += 2 * multiplier;
             } else if (checkerValue.matches("[BCMP]")) {
-                score += 3;
+                score += 3 * multiplier;
             } else if (checkerValue.matches("[FHVWY]")) {
-                score += 4;
+                score += 4 * multiplier;
             } else if (checkerValue.matches("[K]")) {
-                score += 5;
+                score += 5 * multiplier;
             } else if (checkerValue.matches("[JX]")) {
-                score += 8;
+                score += 8 * multiplier;
             } else if (checkerValue.matches("[QZ]")) {
-                score += 10;
+                score += 10 * multiplier;
+            } else if (checkerValue.equals("[")) {
+                multiplier = 3;
+            } else if (checkerValue.equals("{")) {
+                multiplier = 2;
+            } else if (checkerValue.equals("]") ||
+                    checkerValue.equals("}")) {
+                multiplier = 1;
             } else {
                 score += 0;
             }
