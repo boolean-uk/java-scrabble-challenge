1ca2bfba248cd1ea75e3e31ed226e61a4b238180
Osamah Al-maliki <osamah.al-maliki@no.experis.com>
2024-01-12 13:04:17 +0100

    Osamah Al-maliki

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -1,12 +1,74 @@
 package com.booleanuk;
 
 public class Scrabble {
-    public Scrabble(String word) {
 
+    private String word;
+
+    public Scrabble(String word) {
+        this.word = word;
     }
 
     public int score() {
-        return -1;
+        int totalScore = 0;
+
+        int wordMultiplier = 1;
+
+        for (int i = 0; i < word.length(); i++) {
+            char letter = word.toUpperCase().charAt(i);
+
+            if (i < word.length() - 1 && (word.charAt(i + 1) == '{' || word.charAt(i + 1) == '[')) {
+
+                int scoreMultiplier = (word.charAt(i + 1) == '{') ? 2 : (word.charAt(i + 1) == '[') ? 3 : 1;
+
+                i += 2;
+
+                totalScore += getLetterValue(letter) * scoreMultiplier;
+            } else {
+
+                totalScore += getLetterValue(letter);
+            }
+        }
+
+        return totalScore * wordMultiplier;
     }
 
-}
+    private int getLetterValue(char letter) {
+        switch (letter) {
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+            case 'L':
+            case 'N':
+            case 'R':
+            case 'S':
+            case 'T':
+                return 1;
+            case 'D':
+            case 'G':
+                return 2;
+            case 'B':
+            case 'C':
+            case 'M':
+            case 'P':
+                return 3;
+            case 'F':
+            case 'H':
+            case 'V':
+            case 'W':
+            case 'Y':
+                return 4;
+            case 'K':
+                return 5;
+            case 'J':
+            case 'X':
+                return 8;
+            case 'Q':
+            case 'Z':
+                return 10;
+            default:
+                return 0;
+            }
+        }
+    }
