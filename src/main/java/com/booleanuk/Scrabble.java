0cab9bb3cba9026178059aa540847183bdc35ae9
Hassan Hussain <hassanmehmodhussain@gmail.com>
2024-01-12 11:51:13 +0100

    Hassan Hussain

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -1,91 +1,109 @@
 package com.booleanuk;
 
-import java.util.HashMap;
-
 public class Scrabble {
-     String word;
-     int score;
+    String word;
+    int score;
+
     public Scrabble(String word) {
-
-
         this.word = word.toUpperCase();
+    }
 
-
-
-
-
-
+    public int getLetterScore(char letter) {
+        switch (letter) {
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+            case 'L':
+            case 'N':
+            case 'R':
+            case 'S':
+            case 'T':
+                return 1;
+            case 'D':
+            case 'G':
+                return 2;
+            case 'B':
+            case 'C':
+            case 'M':
+            case 'P':
+                return 3;
+            case 'F':
+            case 'H':
+            case 'V':
+            case 'W':
+            case 'Y':
+                return 4;
+            case 'K':
+                return 5;
+            case 'J':
+            case 'X':
+                return 8;
+            case 'Q':
+            case 'Z':
+                return 10;
+            default:
+                return 0;
+        }
     }
 
     public int score() {
-
         score = 0;
+        boolean doubleLetter = false;
+        boolean tripleLetter = false;
+        boolean doubleWord = false;
+        boolean tripleWord = false;
 
         for (int i = 0; i < word.length(); i++) {
             char letter = word.charAt(i);
-            switch (letter) {
-                case 'A':
-                case 'E':
-                case 'I':
-                case 'O':
-                case 'U':
-                case 'L':
-                case 'N':
-                case 'R':
-                case 'S':
-                case 'T':
-                    score += 1;
-                    break;
-                case 'D':
-                case 'G':
-                    score += 2;
-                    break;
-                case 'B':
-                case 'C':
-                case 'M':
-                case 'P':
-                    score += 3;
-                    break;
-                case 'F':
-                case 'H':
-                case 'V':
-                case 'W':
-                case 'Y':
-                    score += 4;
-                    break;
-                case 'K':
-                    score += 5;
-                    break;
-                case 'J':
-                case 'X':
-                    score += 8;
-                    break;
-                case 'Q':
-                case 'Z':
-                    score += 10;
-                    break;
-                default:
-                    score += 0;
 
+
+
+            if (letter == '{') {
+                doubleLetter = true;
+                continue;
+            } else if (letter == '}') {
+                doubleLetter = false;
+                continue;
+            } else if (letter == '[') {
+                tripleLetter = true;
+                continue;
+            } else if (letter == ']') {
+                tripleLetter = false;
+                continue;
+            } else if (letter == '(') {
+                doubleWord = true;
+                continue;
+            } else if (letter == ')') {
+                doubleWord = false;
+                continue;
+            } else if (letter == '<') {
+                tripleWord = true;
+                continue;
+            } else if (letter == '>') {
+                tripleWord = false;
+                continue;
             }
 
+            int letterScore = getLetterScore(letter);
 
+            if (doubleLetter) {
+                letterScore *= 2;
+            } else if (tripleLetter) {
+                letterScore *= 3;
+            }
+
+            score += letterScore;
         }
 
-
-
-
-
-
-
-
-
-
-
-
+        if (doubleWord) {
+            score *= 2;
+        } else if (tripleWord) {
+            score *= 3;
+        }
 
         return score;
     }
-
-
 }
+
