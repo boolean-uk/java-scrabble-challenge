2773edb3eeeb8d7cfaa8683ea31cc48dd5d72996
msg <mats.gravem@no.experis.com>
2024-01-12 09:44:39 +0100

    Core 7/7

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -1,12 +1,28 @@
 package com.booleanuk;
 
 public class Scrabble {
-    public Scrabble(String word) {
+    String letters;
+    int[] letterScore;
 
+    String word;
+
+    public Scrabble(String word) {
+        this.word = word;
+        this.letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        this.letterScore = new int[]{1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
     }
 
     public int score() {
-        return -1;
+        char[] chars = sanitize(word).toUpperCase().toCharArray();
+        int scoreCount = 0;
+
+        for (char l : chars) {
+            scoreCount += letterScore[letters.indexOf(l)];
+        }
+        return scoreCount;
     }
 
+    private String sanitize(String str) {
+        return str.replaceAll("\\s+", "").trim();
+    }
 }
