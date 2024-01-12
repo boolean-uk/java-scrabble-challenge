a5941ef8a4161b5b743d4ef6c36a843bfe7d8714
Ateeb <ateeb@live.no>
2024-01-12 21:35:29 +0100

    Ateeb Salam

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -1,90 +1,78 @@
 package com.booleanuk;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 
 public class Scrabble {
 
-    int score;
-
     String word;
-    public Scrabble(String word) { //q eller quirky
-        this.word=word;
 
+    public Scrabble(String word) {
+        this.word = word;
     }
 
     public int score() {
-        String[] bokstaver1 = {"A", "E", "I", "O", "U", "L", "N", "R", "S", "T"};
-        String[] bokstaver2 = {"D", "G"};
-        String[] bokstaver3 = {"B", "C", "M", "P"};
-        String[] bokstaver4 = {"F", "H", "V", "W", "Y"};
-        String[] bokstaver5 = {"K"};
-        String[] bokstaver6 = {"J", "X"};
-        String[] bokstaver7 = {"Q", "Z"};
+        if (word.contains("!") || word.contains("|")) {
+            return 0;
+        }
 
-        ArrayList<String> wordsList = new ArrayList<>();
+        Map<Character, Integer> letterScores = new HashMap<>();
+        letterScores.put('A', 1); letterScores.put('E', 1); letterScores.put('I', 1);
+        letterScores.put('O', 1); letterScores.put('U', 1); letterScores.put('L', 1);
+        letterScores.put('N', 1); letterScores.put('R', 1); letterScores.put('S', 1);
+        letterScores.put('T', 1); letterScores.put('D', 2); letterScores.put('G', 2);
+        letterScores.put('B', 3); letterScores.put('C', 3); letterScores.put('M', 3);
+        letterScores.put('P', 3); letterScores.put('F', 4); letterScores.put('H', 4);
+        letterScores.put('V', 4); letterScores.put('W', 4); letterScores.put('Y', 4);
+        letterScores.put('K', 5); letterScores.put('J', 8); letterScores.put('X', 8);
+        letterScores.put('Q', 10); letterScores.put('Z', 10);
+
+        if (!isValidMultiplierFormat(word)) {
+            return 0;
+        }
+
         String newW = word.toUpperCase();
-        int wordL = word.length();
-        System.out.println(word);
+        int score = 0;
+        int multiplier = 1;
 
-        for (int i =0; i<wordL; i++){
-            String character = String.valueOf(newW.charAt(i));
-            wordsList.add(character);
+        for (int i = 0; i < newW.length(); i++) {
+            char currentChar = newW.charAt(i);
+
+            if (currentChar == '{') {
+                multiplier *= 2;
+            } else if (currentChar == '}') {
+                multiplier /= 2;
+            } else if (currentChar == '[') {
+                multiplier *= 3;
+            } else if (currentChar == ']') {
+                multiplier /= 3;
+            } else if (letterScores.containsKey(currentChar)) {
+                score += letterScores.get(currentChar) * multiplier;
+            }
         }
 
+        return score;
+    }
 
-        for(int j = 0; j<bokstaver1.length; j++) {
-            for(int i = 0; i<wordsList.size(); i++){
-                if (bokstaver1[j].equals(wordsList.get(i))){
-                    this.score+=1;
+    private boolean isValidMultiplierFormat(String word) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : word.toCharArray()) {
+            if (ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else if (ch == '}' || ch == ']') {
+                if (stack.isEmpty()) {
+                    return false;
                 }
-            }
-        }
-        for(int j = 0; j<bokstaver2.length; j++) {
-            for(int i = 0; i<wordsList.size(); i++){
-                if (bokstaver2[j].equals(wordsList.get(i))){
-                    this.score+=2;
-                }
-            }
-        }
-        for(int j = 0; j<bokstaver3.length; j++) {
-            for(int i = 0; i<wordsList.size(); i++){
-                if (bokstaver3[j].equals(wordsList.get(i))){
-                    this.score+=3;
-                }
-            }
-        }
-        for(int j = 0; j<bokstaver4.length; j++) {
-            for(int i = 0; i<wordsList.size(); i++){
-                if (bokstaver4[j].equals(wordsList.get(i))){
-                    this.score+=4;
-                }
-            }
-        }
-        for(int j = 0; j<bokstaver5.length; j++) {
-            for(int i = 0; i<wordsList.size(); i++){
-                if (bokstaver5[j].equals(wordsList.get(i))){
-                    this.score+=5;
-                }
-            }
-        }
-        for(int j = 0; j<bokstaver6.length; j++) {
-            for(int i = 0; i<wordsList.size(); i++){
-                if (bokstaver6[j].equals(wordsList.get(i))){
-                    this.score+=8;
-                }
-            }
-        }
-        for(int j = 0; j<bokstaver7.length; j++) {
-            for(int i = 0; i<wordsList.size(); i++){
-                if (bokstaver7[j].equals(wordsList.get(i))){
-                    this.score+=10;
+                char openChar = stack.pop();
+                if (ch == '}' && openChar != '{' || ch == ']' && openChar != '[') {
+                    return false;
                 }
             }
         }
 
-
-
-        return this.score;
+        return stack.isEmpty();
     }
 
 }
