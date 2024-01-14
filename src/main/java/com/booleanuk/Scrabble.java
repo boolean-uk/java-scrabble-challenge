190b68e770879d6c802f818113dceb5a80da7869
Alexander <alexander.ellnestam@gmail.com>
2024-01-14 12:15:41 +0100

    Old version

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -1,6 +1,6 @@
 package com.booleanuk;
 
-import java.util.*;
+import java.util.ArrayList;
 
 public class Scrabble {
 
@@ -8,8 +8,8 @@ public class Scrabble {
     public int score = 0;
 
 
-    public String noWord() {
-        return "";
+    public boolean noWord(String word) {
+        return false;
     }
 
     public void noWordScore() {
@@ -18,129 +18,180 @@ public class Scrabble {
 
     public Scrabble(String word) {
 
-        if (Objects.equals(word, noWord())) {
-            noWordScore();
-        } else {
-            score();
+        this.word = word;
+
+    }
+
+    private int maybeMultiply(String word) {
+
+        if (word.startsWith("{") && word.endsWith("}")) {
+            return 3;
         }
-
-        char[] chars = word.toCharArray();
-        boolean usualb = false;
-        boolean curlyb = false;
-        int counter = 1;
-
-
-        for (int n = 0; n < chars.length; n++) {
-
-
-
-
-            if (chars[n] == 'a' || chars[n] == 'A') {
-                this.score += counter;
-            }
-
-            if (chars[n] == 'e' || chars[n] == 'E') {
-                this.score += counter;
-            }
-            if (chars[n] == 'i' || chars[n] == 'I') {
-                this.score += counter;
-            }
-            if (chars[n] == 'o' || chars[n] == 'O') {
-                this.score += 1*counter;
-            }
-            if (chars[n] == 'u' || chars[n] == 'U') {
-                this.score += counter;
-            }
-            if (chars[n] == 'l' || chars[n] == 'L') {
-                this.score += counter;
-            }
-            if (chars[n] == 'n' || chars[n] == 'N') {
-                this.score += counter;
-            }
-            if (chars[n] == 'r' || chars[n] == 'R') {
-                this.score += counter;
-            }
-            if (chars[n] == 's' || chars[n] == 'S') {
-                this.score += counter;
-            }
-            if (chars[n] == 't' || chars[n] == 'T') {
-                this.score += counter;
-            }
-            if (chars[n] == 'd' || chars[n] == 'D') {
-                this.score += 2 * counter;
-            }
-            if (chars[n] == 'g' || chars[n] == 'G') {
-                this.score += 2 * counter;
-            }
-            if (chars[n] == 'b' || chars[n] == 'B') {
-                this.score += 3 * counter;
-            }
-            if (chars[n] == 'c' || chars[n] == 'C') {
-                this.score += 3 * counter;
-            }
-            if (chars[n] == 'm' || chars[n] == 'M') {
-                this.score += 3 * counter;
-            }
-            if (chars[n] == 'p' || chars[n] == 'P') {
-                this.score += 3 * counter;
-            }
-            if (chars[n] == 'f' || chars[n] == 'F') {
-                this.score += 4*counter;
-            }
-            if (chars[n] == 'h' || chars[n] == 'H') {
-                this.score += 4*counter;
-            }
-            if (chars[n] == 'v' || chars[n] == 'V') {
-                this.score += 4*counter;
-            }
-            if (chars[n] == 'w' || chars[n] == 'W') {
-                this.score += 4*counter;
-            }
-            if (chars[n] == 'y' || chars[n] == 'Y') {
-                this.score += 4*counter;
-            }
-            if (chars[n] == 'k' || chars[n] == 'K') {
-                this.score += 5*counter;
-            }
-            if (chars[n] == 'j' || chars[n] == 'J') {
-                this.score += 8*counter;
-            }
-            if (chars[n] == 'x' || chars[n] == 'X') {
-                this.score += 8*counter;
-            }
-            if (chars[n] == 'q' || chars[n] == 'Q') {
-                this.score += 10*counter;
-            }
-            if (chars[n] == 'z' || chars[n] == 'Z') {
-                this.score += 10*counter;
-            }
-
-//            if (chars[n] == '[' || chars[n] == ']') {
-//                usualb = !usualb;
-//            }
-//
-//            if(usualb){
-//                counter = 2;
-//            }
-
-
-
+        if (word.startsWith("[") && word.endsWith("]")) {
+            return 2;
         }
+        return 1;
+
+    }
+
+    private boolean isTriple(String word) {
+        return word.startsWith("{") && word.endsWith("}");
+    }
 
 
-
-
-
-
+    private boolean isDouble(String word) {
+        return word.startsWith("[") && word.endsWith("]");
     }
 
 
     public int score() {
 
-        return this.score;
+        if (containsInvalidChars(word)) {
+            return 0;
+        }
+        String wordTemp = this.word;
+
+        if (noWord(wordTemp)) {
+            return 0;
+        }
+
+
+
+        char[] chars = word.toLowerCase().toCharArray();
+        int counter = 1;
+
+        ArrayList<Character> parens = new ArrayList<>();
+        for (int i = 0; i < chars.length; i++) {
+            char aChar = chars[i];
+            if (startsMultplier(aChar)){
+                parens.add(aChar);
+            }
+
+            if (aChar == '}' && parens.isEmpty()){
+                return 0;
+            }
+
+            if (aChar == ']' && parens.isEmpty()){
+                return 0;
+            }
+
+            if ((aChar == '}') && parens.get(parens.size()-1) == '{'){
+                parens.remove(parens.size()-1);
+            }
+
+            if ((aChar == ']') && parens.get(parens.size()-1) == '['){
+                parens.remove(parens.size()-1);
+            }
+
+        }
+
+        if (!parens.isEmpty()){
+            return 0;
+        }
+
+        int letterMultiplier = 1;
+
+        int wordMultiplier = 1;
+        int totalScore = 0;
+
+
+        for (int n = 0; n < chars.length; n++) {
+            char aChar = chars[n];
+
+
+            if (startsMultplier(aChar)) {
+                if (wordMultiplier == 1 && startsMultplier(chars[n + 1])) {
+                    wordMultiplier = aChar == '[' ? 3 : 2;
+
+                    continue;
+                }
+
+                letterMultiplier = aChar == '[' ? 3 : 2;
+                continue;
+            }
+
+            if (aChar == '}' || aChar == ']') {
+                continue;
+            }
+
+
+            if (aChar == 'a'
+                    || aChar == 'e'
+                    || aChar == 'i'
+                    || aChar == 'u'
+                    || aChar == 'l'
+                    || aChar == 'n'
+                    || aChar == 'r'
+                    || aChar == 's'
+                    || aChar == 't'
+                    || aChar == 'o') {
+                totalScore += letterMultiplier;
+                continue;
+            }
+
+            if (aChar == 'd' || aChar == 'g') {
+                totalScore += 2 * letterMultiplier;
+                continue;
+            }
+            if (aChar == 'b'
+                    || aChar == 'c'
+                    || aChar == 'm'
+                    || aChar == 'p'
+
+
+            ) {
+                totalScore += 3 * letterMultiplier;
+                continue;
+            }
+
+            if (aChar == 'f'
+                    || aChar == 'h'
+                    || aChar == 'v'
+                    || aChar == 'w'
+                    || aChar == 'y'
+            ) {
+                totalScore += 4 * letterMultiplier;
+                continue;
+            }
+
+            if (aChar == 'k') {
+                totalScore += 5 * letterMultiplier;
+                continue;
+            }
+            if (aChar == 'j' || aChar == 'x') {
+                totalScore += 8 * letterMultiplier;
+                continue;
+            }
+            if (aChar == 'q' || aChar == 'z') {
+                totalScore += 10 * letterMultiplier;
+
+
+            }
+
+
+        }
+
+
+        return totalScore * wordMultiplier;
+    }
+
+    private boolean containsInvalidChars(String word) {
+        return word.contains("!") || word.contains("|");
+    }
+
+    private static boolean startsMultplier(char aChar) {
+        return aChar == '[' || aChar == '{';
+    }
+
+    private String removeWordMultipliers(String wordTemp) {
+        return wordTemp.substring(1, wordTemp.length() - 1);
     }
 
 
+    public static void main(String[] args) {
+        Scrabble sc = new Scrabble("d[o]g");
 
+        System.out.println(sc.score());
+    }
 
 }
